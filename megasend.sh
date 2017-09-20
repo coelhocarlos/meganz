c75e2fc7db0e91d7a8264e57559dd100a935e8a8
coelhocarlos <carloscoelho_@msn.com>
2017-09-20 12:16:34 -0300

    Update megasend.sh

--- megasend.sh
+++ megasend.sh
@@ -1,5 +1,8 @@
-
+############################
 cd /media/hd-ext/
 tar -zcvf bkp-`date +%d-%m-%Y--%H-%M`.tar.gz mega/ 
 megaput *.tar.gz
 rm *.tar.gz
+
+# use crontab -e 
+# 0 */6 * * * /path-to-script //a cada seis horas
